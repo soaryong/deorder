d29f1e73fe3ba5ea558d2811c2b8bef87a020640
soaryong <soaryong.c@gmail.com>
2024-12-08 20:10:19 +0900

    pb url

--- api/pocketbase.ts
+++ api/pocketbase.ts
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL);
+const pb = new PocketBase("https://api.tuktuk.today");
 
 if (process.env.NODE_ENV === 'development') pb.autoCancellation(false);
 
